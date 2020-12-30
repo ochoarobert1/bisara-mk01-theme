dbb59ae3d19cbe157c8d5698522724ce36a6027e
Robert Ochoa <ochoa.robert1@gmail.com>
2020-12-30 15:12:35 -0400

    Inicializacion

---
+++ includes/wp_custom_post_type.php
@@ -0,0 +1,60 @@
+<?php
+/*
+function bisara_custom_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Clientes', 'Post Type General Name', 'bisara' ),
+		'singular_name'         => _x( 'Cliente', 'Post Type Singular Name', 'bisara' ),
+		'menu_name'             => __( 'Clientes', 'bisara' ),
+		'name_admin_bar'        => __( 'Clientes', 'bisara' ),
+		'archives'              => __( 'Archivo de Clientes', 'bisara' ),
+		'attributes'            => __( 'Atributos de Cliente', 'bisara' ),
+		'parent_item_colon'     => __( 'Cliente Padre:', 'bisara' ),
+		'all_items'             => __( 'Todos los Clientes', 'bisara' ),
+		'add_new_item'          => __( 'Agregar Nuevo Cliente', 'bisara' ),
+		'add_new'               => __( 'Agregar Nuevo', 'bisara' ),
+		'new_item'              => __( 'Nuevo Cliente', 'bisara' ),
+		'edit_item'             => __( 'Editar Cliente', 'bisara' ),
+		'update_item'           => __( 'Actualizar Cliente', 'bisara' ),
+		'view_item'             => __( 'Ver Cliente', 'bisara' ),
+		'view_items'            => __( 'Ver Clientes', 'bisara' ),
+		'search_items'          => __( 'Buscar Cliente', 'bisara' ),
+		'not_found'             => __( 'No hay resultados', 'bisara' ),
+		'not_found_in_trash'    => __( 'No hay resultados en Papelera', 'bisara' ),
+		'featured_image'        => __( 'Imagen del Cliente', 'bisara' ),
+		'set_featured_image'    => __( 'Colocar Imagen del Cliente', 'bisara' ),
+		'remove_featured_image' => __( 'Remover Imagen del Cliente', 'bisara' ),
+		'use_featured_image'    => __( 'Usar como Imagen del Cliente', 'bisara' ),
+		'insert_into_item'      => __( 'Insertar en Cliente', 'bisara' ),
+		'uploaded_to_this_item' => __( 'Cargado a este Cliente', 'bisara' ),
+		'items_list'            => __( 'Listado de Clientes', 'bisara' ),
+		'items_list_navigation' => __( 'Navegación del Listado de Cliente', 'bisara' ),
+		'filter_items_list'     => __( 'Filtro del Listado de Clientes', 'bisara' ),
+	);
+	$args = array(
+		'label'                 => __( 'Cliente', 'bisara' ),
+		'description'           => __( 'Portafolio de Clientes', 'bisara' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'            => array( 'tipos-de-clientes' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 15,
+		'menu_icon'             => 'dashicons-businessperson',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+		'show_in_rest'          => true,
+	);
+	register_post_type( 'clientes', $args );
+
+}
+
+add_action( 'init', 'bisara_custom_post_type', 0 );
+*/
